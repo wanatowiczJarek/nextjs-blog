a43a656bbb63d2134e0a25caa7c85e9578db1cff
Jarosław Wanatowicz <jwanatowicz@sii.pl>
2023-08-09 23:20:50 +0200

    added theme switch feature + tailwind config + clean initial app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,13 +11,17 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
+        <Providers>
+        {children}
+        </Providers>
+        </body>
     </html>
   )
 }
